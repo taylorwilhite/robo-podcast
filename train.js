b843ba24054cb63aafecc965930bb707b007632f
Taylor Wilhite <taylorwilhite@gmail.com>
2020-01-24 13:40:35 -0800

    update trainingdata format

--- train.js
+++ train.js
@@ -2,13 +2,22 @@ const brain = require('brain.js');
 const {transcripts} = require('./transcripts');
 const fs = require('fs');
 
-const trainingData = transcripts;
+const buildData = (data) => {
+  let output = []
+  for (let i = 1; i < data.length; i++) {
+    output.push({input: data[i - 1], output: data[i]})
+  }
+
+  return output;
+};
+
+const trainingData = buildData(transcripts);
 
 const net = new brain.recurrent.LSTM();
 const result = net.train(trainingData, {
-  iterations: 1500,
+  iterations: 10000,
   log: details => console.log(details),
-  errorThresh: 0.065
+  errorThresh: 0.025
 });
 
 fs.writeFileSync('trained-net.js', `exports.trainedNet = ${ net.toFunction().toString() };`);
